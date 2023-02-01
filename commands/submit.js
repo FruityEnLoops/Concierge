3b745586f79e2a16001c4e66b7e15398d0b3d76f
Loïc Demay <fruityenloops@gmail.com>
2023-02-01 16:16:08 +0100

    Add defaults query during submission and consistency checks

--- commands/submit.js
+++ commands/submit.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { gameTypes } = require('../constants/GameType');
 const { songlist } = require('../meta.json');
+const { getDefaults } = require('../utils/db.js')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -27,22 +28,39 @@ module.exports = {
 			option.setName("image")
 			.setDescription("Image du score")
 				.setRequired(true)
-		),
+		)
+		.addBooleanOption(option =>
+			option.setName("alphanef")
+			.setDescription("Score joué à l'Alphanef")
+			.setRequired(false)),
 	async execute(interaction) {
+		let defaults = await getDefaults(interaction.user.tag.replace("#", ""))
+		if(!defaults) {
+			interaction.reply({ content: "Merci d'utiliser la commande `/setup` pour configurer vos préférences d'envoi par défaut.", ephemeral: true })
+			return;
+		}
+		if(!songlist.find(song => `${song.song} [${song.difficulty}]` === interaction.options.getString("song") &&
+			song.gametype === interaction.options.getString("jeu"))) {
+			interaction.reply({ content: "La song choisie est introuvable ou ne correspond pas au jeu choisi. Merci de bien vouloir revérifier la commande.", ephemeral: true });
+			return;
+		}
+		if(interaction.options.getBoolean("alphanef") != null) {
+			defaults.alphanef = interaction.options.getBoolean("alphanef") ? 1 : 0;
+		}
 		let score = {
 			id: interaction.user.tag.replace("#", ""),
-			pseudo: "TODO",
+			pseudo: defaults.pseudo,
 			song: interaction.options.getString("song"),
 			score: interaction.options.getInteger("score"),
 			image: interaction.options.getAttachment("image").url,
-			alphanef: 0
+			alphanef: defaults.alphanef
 		};
-		await interaction.reply("```json\n" + JSON.stringify(score) + "\n```");
+		await interaction.reply("POST endpoint?game=" + interaction.options.getString("jeu") + "\n```json\n" + JSON.stringify(score) + "\n```");
 	},
 	async autocomplete(interaction) {
 		interaction.respond(
 			songlist.filter(
-				song => song.song.includes(interaction.options.getFocused()) &&
+				song => `${song.song} [${song.difficulty}]`.includes(interaction.options.getFocused()) &&
 					song.gametype === interaction.options.getString("jeu")
 				)
 				.map(song => (
--- utils/db.js
+++ utils/db.js
@@ -6,11 +6,15 @@ module.exports = {
         db.run("INSERT INTO defaults VALUES (?, ?, ?) ON CONFLICT(tag) DO UPDATE SET alphanef = ?, pseudo = ?", [tag, alphanef, pseudo, alphanef, pseudo]);
         db.close();
     },
-    async getDefautls(tag) {
-        const db = new sqlite.Database("./defaults.db");
-        db.get(`SELECT * FROM defaults WHERE tag = ${tag}`, (row) => {
-            db.close();
-            return row;
+    async getDefaults(tag) {
+        return new Promise((resolve, reject) => {
+            const db = new sqlite.Database("./defaults.db");
+            db.get(`SELECT * FROM defaults WHERE tag = ?`, tag, (error, row) => {
+                if(error) {
+                    return reject(error);
+                }
+                return resolve(row);
+            })
         })
     }
 }
